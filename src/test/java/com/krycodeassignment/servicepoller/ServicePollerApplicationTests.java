434b90cc747e3e82bb577d3b42d80757fbf020a6
Isaac Jimenez <izakjimenez@gmail.com>
2021-11-15 20:23:37 -0600

    Project initialization

---
+++ src/test/java/com/krycodeassignment/servicepoller/ServicePollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krycodeassignment.servicepoller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
